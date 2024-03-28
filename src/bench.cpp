a63235b5c44b3fb11b89ae517c53e3676df51e9f
Andreas <codr7@protonmail.com>
2024-03-28 01:08:22 +0100

    Move benchmarks.

--- src/bench.cpp
+++ src/bench.cpp
@@ -1,66 +1,10 @@
 #include <iostream>
-#include <set>
-
-#include "bset.hpp"
-#include "bin_set.hpp"
 #include "timer.hpp"
 
 using namespace std;
 
-static const auto REPS = 10;
-static const auto BIN_SET_MAX = 10000;
-
-// 23ms
-
-static void bset() {
-  BSet<int> s;
-
-  for (auto i = BIN_SET_MAX; i >= 0; i--) {
-    assert(s.insert(i));
-    assert(s.contains(i));
-  }
-}
-
-static void bin_set() {
-  BinSet<int> s;
-
-  for (auto i = BIN_SET_MAX; i >= 0; i--) {
-    assert(s.insert(i));
-    assert(s.contains(i));
-  }
-}
-
-static void std_set() {
-  set<int> s;
-
-  for (auto i = BIN_SET_MAX; i >= 0; i--) {
-    auto [_, ok] = s.insert(i);
-    assert(ok);
-    assert(s.contains(i));
-  }
-}
-
 int main() {
   Timer t;
-
-  for (auto i = 0; i < REPS; i++) {
-    bset();
-  }
-
-  cout << "BSet " << t.ms() << endl;
-  t.reset();
-
-  for (auto i = 0; i < REPS; i++) {
-    bin_set();
-  }
-
-  cout << "BinSet " << t.ms() << endl;
-  t.reset();
-
-  for (auto i = 0; i < REPS; i++) {
-    std_set();
-  }
-
-  cout << "std::set " << t.ms() << endl;
+  cout << t.ms() << endl;
   return 0;
 }
